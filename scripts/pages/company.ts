41aeab212fa4a865f8131c8331e2acdd749da01f
Sean Jezewski <sean@moovweb.com>
2013-11-24 15:12:07 -0800

    Updating fields per company. Spent a while fighting global else() statements ... something weird there

--- scripts/pages/company.ts
+++ scripts/pages/company.ts
@@ -18,12 +18,38 @@ $("//body") {
     
     $employees = fetch("//*[@id='num_employees']/text()")
     $("//div[@class='people']/span") {
-      text($employees)
+      match($employees, "") {
+        remove("..")
+      } else() {
+        text($employees)
+      }
+    }
+
+    $founded = fetch("//td[text()='Founded']/following-sibling::td/text()")
+    log("founded?" + $founded)
+    $("//div[@class='founded']/span") {
+      match($founded) { #, "") {
+        with("") {
+          remove("..")
+        } else() {
+          text($founded)
+        }
+      }
     }
 
     $funding = fetch("//strong[contains(text(),'FUNDING TOTAL')]/../../td[last()]/strong/text()")
     $("//div[@class='funding']/span") {
       text($funding)
+    }
+
+    $("//div[@id='company_logo']//img") {
+      move_to("//div[@class='info logo']")
+    }
+
+    $("//h2[contains(text(), 'Tags')]/following-sibling::div//a[contains(@href,'tag')]") {
+      wrap("span") {
+        move_to("//div[@class='info tags']")
+      }
     }
 
 
